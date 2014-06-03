94546ba9b1a47bc7ac13c6b676ae5b00bb894c7b
Arthemus C. Moreira <arthemus@outlook.com>
2014-06-03 15:55:39 -0300

    Separação do teste de consulta pela chave de acesso.

--- nfe-api/src/test/java/nfe/produces/api/LeituraDeArquivoTest.java
+++ nfe-api/src/test/java/nfe/produces/api/LeituraDeArquivoTest.java
@@ -1,28 +1,12 @@
 package nfe.produces.api;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.rmi.RemoteException;
-
-import javax.xml.stream.XMLStreamException;
-
 import nfe.produces.api.utils.CertificadoFactory;
 
-import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.util.AXIOMUtil;
 import org.junit.Assert;
 import org.junit.Test;
-import org.nfe.certificado.Certificado;
 import org.nfe.certificado.CertificadoDigital;
-import org.nfe.certificado.CertificadoValidateException;
 import org.nfe.commons.Ambiente;
 import org.nfe.commons.UF;
-
-import br.inf.portalfiscal.www.nfe.wsdl.nfedownloadnf.NfeCabecMsg;
-import br.inf.portalfiscal.www.nfe.wsdl.nfedownloadnf.NfeCabecMsgE;
-import br.inf.portalfiscal.www.nfe.wsdl.nfedownloadnf.NfeDadosMsg;
-import br.inf.portalfiscal.www.nfe.wsdl.nfedownloadnf.NfeDownloadNFResult;
-import br.inf.portalfiscal.www.nfe.wsdl.nfedownloadnf.NfeDownloadNFStub;
 
 /**
  * 
@@ -58,32 +42,4 @@ public class LeituraDeArquivoTest {
 		}		
 	}
 	
-	public void testaConsultaNFPelaChaveDeAcesso() 
-			throws MalformedURLException, XMLStreamException, RemoteException, CertificadoValidateException {
-		CertificadoDigital certificado = CertificadoFactory.getCertificadoLocalValido();		
-		//--Em testes, necessita dos arquivos xsd do layout de download do arquivo xml...
-		Certificado.validate(certificado);						
-		NfeDownloadNFResult result = new NfeDownloadNFResult();
-
-		String Dados = "<?xml version='1.0' encoding='UTF-8' ?>"
-				+ "<downloadNFe xmlns='http://www.portalfiscal.inf.br/nfe' versao='1.00'>"
-				+ "<tpAmb>1</tpAmb>"
-				+ "<xServ>DOWNLOAD NFE</xServ>" 
-				+ "<CNPJ>02609944000196</CNPJ>"
-				+ "<chNFe>35130546928552000165550010004291241002641341</chNFe>" 
-				+ "</downloadNFe>";
-
-		OMElement ome = AXIOMUtil.stringToOM(Dados.trim());
-		NfeDadosMsg dadosMsg = new NfeDadosMsg();
-		dadosMsg.setExtraElement(ome);
-		NfeCabecMsg nfeCabecMsg = new NfeCabecMsg();
-		nfeCabecMsg.setCUF("43");
-		nfeCabecMsg.setVersaoDados("1.00");
-		NfeCabecMsgE nfeCabecMsgE = new NfeCabecMsgE();
-		nfeCabecMsgE.setNfeCabecMsg(nfeCabecMsg);
-		URL url = new URL("https://nfe.sefaz.rs.gov.br/ws/nfeDownloadNF/nfeDownloadNF.asmx");
-		NfeDownloadNFStub stub = new NfeDownloadNFStub(url.toString());
-		result = stub.nfeDownloadNF(dadosMsg, nfeCabecMsgE);
-		result.getExtraElement().toString();				
-	}
 }
